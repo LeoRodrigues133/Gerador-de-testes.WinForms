1675858db362427ab182f566e7862d365b411d08
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-06-12 19:12:20 -0300

    Criado Módulo Compartilhado

---
+++ GeradorDeTestes/GeradorDeTestes.WinApp/1 - Módulo Compartilado/ControladorBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.WinApp._1___Módulo_Compartilado
+{
+    public abstract class ControladorBase
+    {
+        public abstract UserControl ObterListagem();
+
+        public abstract void Adicionar();
+        public abstract void Editar();
+        public abstract void Excluir();
+    }
+}
