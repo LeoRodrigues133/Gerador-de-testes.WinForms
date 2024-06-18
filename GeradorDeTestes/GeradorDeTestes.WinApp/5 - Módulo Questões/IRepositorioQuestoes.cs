e43afc416fe611a514d28cd87acd41bcf7b4a9bc
Leonardo Rodrigues <himata666@hotmail.com>
2024-06-18 18:03:55 -0300

    Iniciado módulo de questões;

---
+++ GeradorDeTestes/GeradorDeTestes.WinApp/5 - Módulo Questões/IRepositorioQuestoes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.WinApp._5___Módulo_Questões
+{
+    public interface IRepositorioQuestoes
+    {
+        void Cadastrar(Questoes novaQuestao);
+        bool Editar(int id, Disciplinas atualizarDisciplina);
+        bool Excluir(int id);
+        Disciplinas SelecionarPorId(int id);
+        List<Disciplinas> SelecionarTodos();
+    }
+}
---
+++ GeradorDeTestes/GeradorDeTestes.WinApp/5 - Módulo Questões/Questao.cs
@@ -0,0 +1,28 @@
+﻿using GeradorDeTestes.WinApp._1___Módulo_Compartilado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTestes.WinApp._5___Módulo_Questões
+{
+    public class Questao : EntidadeBase
+    {
+        public Materia materia;
+        public string Enunciado {  get; set; }
+        public Questao()
+        {
+            
+        }
+        public override void AtualizarRegistro(EntidadeBase novoRegistro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<string> Validar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
