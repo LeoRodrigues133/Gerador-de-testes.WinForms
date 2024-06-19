47b51a937a40c45dd7dcbf767cfb38e3b8888966
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-06-19 06:57:37 -0300

    Debugado e provavlemente o bug esta no metodo editar em repositorioBaseEmArquivo

--- GeradorDeTestes/GeradorDeTestes.WinApp/1 - Módulo Compartilado/RepositorioBaseEmArquivo.cs
+++ GeradorDeTestes/GeradorDeTestes.WinApp/1 - Módulo Compartilado/RepositorioBaseEmArquivo.cs
@@ -31,7 +31,7 @@ namespace GeradorDeTestes.WinApp._1___Módulo_Compartilado
             contexto.Gravar();
         }
 
-        public bool Editar(int id, T novaEntidade)
+        public bool Editar(int id, T novaEntidade) //Bug a resolver
         {
             T registro = SelecionarPorId(id);
 
